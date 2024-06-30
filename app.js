7e596c37edd17b8d4779a4d7b810daa0efcbe10e
Harin-Mabao <mixharinmabao@gmail.com>
2024-07-01 02:55:00 +0700

    Update app.js Rev00 First issued

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-// write code here
+let a = 1 * '4' + +null + +' ' - '5' * 2 + +(7 + 2 + '' + +!!undefined);
+
+console.log('Result a = ',a)
+
+let a1 = 1 * '4'
+let a2 = +null
+let a3 = +' '
+let a4 = -'5' * 2
+let a5 = +(7 + 2 + '' + +!!undefined)
+
+console.log("Result 1 * '4' = ", a1)
+console.log('Result +null = ',a2)
+console.log("Result +' ' = ",a3)
+console.log("Result '5' * 2 = ",a4)
+console.log("Result 7 + 2 + '' +!!undefined = ",a5)
+console.log(`Recheck a = ${a1} + ${a2} + ${a3} + ${a4} + ${a5} = `, a1 + a2 + a3 + a4 + a5)
